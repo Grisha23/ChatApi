a4645b0f923d82f19f5f148d2608b058db5f9df9
Grisha23 <gzharkikh@mail.ru>
2019-02-01 19:04:35 +0300

    rm todo

--- TestTask/settings.py
+++ TestTask/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     # 'django.contrib.staticfiles',
     'chat_app',
 ]
-#todo: csrf??
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
